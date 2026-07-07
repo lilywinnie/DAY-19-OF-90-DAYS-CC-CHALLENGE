b291b9039ae4f0330aebb7a80c0150108de279cc
Shakira <287145569+lilywinnie@users.noreply.github.com>
2026-07-07 07:32:46 +0600

    Create Ans_2.cpp

---
+++ Ans_2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int P1,P2,P3,P4;
+    cin>>P1>>P2>>P3>>P4;
+    
+    int count=0;
+    
+    if (P1 >= 10) count++;
+    if (P2 >= 10) count++;
+    if (P3 >= 10) count++;
+    if (P4 >= 10) count++;
+    
+    cout<<count;
+
+    return 0;
+}
