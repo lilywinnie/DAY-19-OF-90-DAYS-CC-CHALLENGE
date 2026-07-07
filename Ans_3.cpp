70df065a840baa729c88e2fdb83a106de1563255
Shakira <287145569+lilywinnie@users.noreply.github.com>
2026-07-07 07:38:36 +0600

    Create Ans_3.cpp

---
+++ Ans_3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    
+    if (2 * M >= N) {
+        cout << "NEWBIE\n";
+    } else {
+        cout << "PRO\n";
+    }
+    
+    return 0;
+}
